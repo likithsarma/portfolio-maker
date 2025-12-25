1dcf89d36623f0cac12d6dc61c169bad4a02fae4
Likith <likithjandhyala@gmail.com>
2025-12-25 20:10:48 +0530

    temporary changes

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']  # Allow any domain (change to specific domains in production)
 
 # Application definition
