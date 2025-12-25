039ab94656f88cddf6ae307844f1411cd779a469
Likith <likithjandhyala@gmail.com>
2025-12-25 19:40:34 +0530

    point api to render backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/"; // for local dev
+
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: API_BASE_URL,
 });
